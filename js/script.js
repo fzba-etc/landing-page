91bc28d1ca13e40520188785675d013ddd3fc3a6
Fiza <fizabaloch.071@gmail.com>
2025-03-15 23:45:04 -0600

    Test commit.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ function toggleVideo() {
     video.pause();
     trailer.classList.toggle('active');
 }
+
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelector('.content');
+    banner.style.background = `url("../images/videos/thumbnails/${bg})`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    })
+}
